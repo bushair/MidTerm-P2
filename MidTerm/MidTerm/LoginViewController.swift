002ee372517e13341072a8cf0fda850f10fe1c7e
Bushair <bushairkm007@gmail.com>
2019-11-06 23:46:29 -0500

    Users Plist Created

--- MidTerm/MidTerm/LoginViewController.swift
+++ MidTerm/MidTerm/LoginViewController.swift
@@ -8,12 +8,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class LoginViewController: UIViewController
+{
+    
+    @IBOutlet weak var txtUserName: UITextField!
+    
+    @IBOutlet weak var txtPassword: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    @IBAction func btnLogin(_ sender: Any) {
+    }
+    
+     let userDefault = UserDefaults.standard
 
 
 }
