8f439c77d4a3a1886afd56fe2f503118599cc8bb
Bushair <bushairkm007@gmail.com>
2019-11-06 21:26:29 -0500

    Initial Commit

---
+++ MidTerm-P2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MidTerm-P2
+//
+//  Created by Akhil Joseph on 2019-11-06.
+//  Copyright © 2019 Bushair. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
