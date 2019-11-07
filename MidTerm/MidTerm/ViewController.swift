f101367fc8640ade453fc4795c318fc8cee726b4
Bushair <bushairkm007@gmail.com>
2019-11-06 22:05:50 -0500

    App Icon Added

---
+++ MidTerm/MidTerm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MidTerm
+//
+//  Created by Akhil Joseph on 2019-11-06.
+//  Copyright © 2019 Bushair. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
